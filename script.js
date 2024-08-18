d7163bcb7f83b65b82c1a89146ac06117ae89be2
Jamil-4khtar <156568813+Jamil-4khtar@users.noreply.github.com>
2024-08-18 23:59:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 function getFormvalue() {
     //Write your code here
+	// Select the form
+const form = document.querySelector('form');
+
+// Add submit event listener
+form.addEventListener('submit', function(event) {
+  // Prevent default form submission
+  event.preventDefault();
+
+  // Access form data
+  const firstName = event.target.elements.firstName.value;
+  const lastName = event.target.elements.lastName.value;
+
+  // Display the data
+  alert(firstName + ' ' + lastName);
+});
 
 }
