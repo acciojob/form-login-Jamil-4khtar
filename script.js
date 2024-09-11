25bce62ffa30885506a542583af865fe4801dc36
Jamil-4khtar <jamil4khtar@gmail.com>
2024-09-11 18:24:10 +0530

    Update script

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
-function getFormvalue() {
+let s_first = document.getElementsByName("fname")[0]
+let s_sec = document.getElementsByName("lname")[0]
+let s_submit = document.querySelector('input[type="submit"]')
+let s_form = document.querySelector("form")
+
+
+s_submit.addEventListener("click", (e) => {
+  e.preventDefault();
+  let first = s_first.value;
+  let second = s_sec.value;
+
+  getFormvalue(first, second);
+
+  
+})
+function getFormvalue(first, second) {
     //Write your code here
-	// Select the form
-const form = document.querySelector('form');
-
-// Add submit event listener
-form.addEventListener('submit', function(event) {
-  // Prevent default form submission
-  event.preventDefault();
-
-  // Access form data
-  const firstName = event.target.elements.firstName.value;
-  const lastName = event.target.elements.lastName.value;
-
-  // Display the data
-  alert(firstName + ' ' + lastName);
-});
+    alert(first + " " + second);
 
 }
